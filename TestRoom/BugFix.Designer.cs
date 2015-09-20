3ce32932a6ee07a2c71d40f439f58e94c588c908
4wdtrip <kyip08@gmail.com>
2015-09-20 11:20:03 +1000

    Change button to "BugFix"

--- TestRoom/BugFix.Designer.cs
+++ TestRoom/BugFix.Designer.cs
@@ -28,11 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(455, 187);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Fix IT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BugFix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 481);
+            this.Controls.Add(this.button1);
+            this.Name = "BugFix";
             this.Text = "BugFix";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
